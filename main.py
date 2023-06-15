a142aff601bd0c3300e0907b98016cb6472fcb29
kev1nweng <95159615+kev1nweng@users.noreply.github.com>
2023-06-15 19:23:58 +0800

    更有故事感的提示 & 完整的进阶玩法！！
    Co-authored-by: Playmaker <roaringflame114@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from time import sleep
 from os import system as run
 import random
 
-version = "1.4.0"
+version = "2.0.0"
 
 
 def clearScn():
@@ -29,15 +30,17 @@ class msgHandler:
 # 初始化玩家列表
 playerArray = []
 swordsArray = [25, 45, 50, 65, 80]
+counterAttackArray = [80, 85, 90, 95, 95]
 
 msgHandler.banner("Chivalry's Oath - 开始")
 msgHandler.title("Chivalry's Oath - " + version)
+msgHandler.info("Never compromise with your enemies.")
 
 while True:
     try:
         playerCount = int(input("请输入玩家数量\n>> "))
         if playerCount < 2 or playerCount % 2 == 0:
-            raise Exception("玩家数量过少或输入非奇数")
+            raise Exception("用学者的话来说：玩家数量过少或输入非奇数")
         break
     except Exception as e:
         msgHandler.warn(str(e))
@@ -45,16 +48,17 @@ while True:
 
 msgHandler.banner("Chivalry's Oath - 玩家设置")
 
-playerArray.append([0, "裁判", 0])
+playerArray.append([0, "裁判", 0, 0])
 for i in range(playerCount):
     while True:
         try:
-            playerArray.append([0, "", 0])
+            msgHandler.banner("Chivalry's Oath - 玩家设置")
+            playerArray.append([0, "", 0, 0])
             currentPlayer = playerArray[i + 1]
             currentPlayer[0] = i + 1  # 玩家ID
             currentPlayer[1] = input("\n请输入玩家" + str(i + 1) + "的名字\n>> ")  # 玩家名称
             if currentPlayer[1] == "":
-                raise Exception("玩家名字为空")
+                raise Exception("玩家名字为空…… 这是最后记住他们名字的机会了。")
             currentPlayer[2] = input(
                 "\n请输入玩家 " + str(i + 1) + " 选择的剑\n选择范围：25/45/50/65/80/random\n>> "
             )
@@ -63,13 +67,32 @@ for i in range(playerCount):
                 print("选出的剑：" + str(currentPlayer[2]))
             currentPlayer[2] = int(currentPlayer[2])
             if not currentPlayer[2] in [25, 45, 50, 65, 80]:
-                raise Exception("不合法的剑：超出选择范围")
-            # TODO: 重复武器重新分配
+                raise Exception("不合法的剑：看来你的选择犹如你的人生一样不够明智")
+            for selectedSword in swordsArray:
+                if currentPlayer[2] == selectedSword:
+                    currentPlayer[3] = counterAttackArray[i]
             break
         except Exception as e:
             msgHandler.warn(str(e))
             playerArray.remove(currentPlayer)  # 清理错误产生的无效玩家
             continue
+
+playerSwordArray = []
+for k in range(len(playerArray)):
+    playerSwordArray.append(playerArray[k][2])
+
+if len(playerSwordArray) != len(set(playerSwordArray)):
+    msgHandler.banner("Chivalry's Oath - 重复的剑")
+    msgHandler.info("注意，注意！有玩家选择了重复的剑！让我们看看祝福之泉会赋予他们什么宝物：")
+    for x in range(len(playerSwordArray)):
+        for y in range(x + 1, len(playerSwordArray)):
+            if playerSwordArray[x] == playerSwordArray[y]:
+                playerSwordArray[y] = random.choice(swordsArray)
+    for t in range(len(playerSwordArray)):
+        playerArray[t][2] = playerSwordArray[t]
+    print("\n", playerArray)
+    input("\n按下 Enter 确认并开始游戏\n>>")
+
 
 msgHandler.banner("Chivalry's Oath")
 
@@ -110,9 +133,9 @@ while True:
                         playerLocated = True
                         break
                 if targetPlayer[0] == playerOnStage[0]:
-                    raise Exception("你不能攻击自己！")
+                    raise Exception("自杀是不被允许的……")
                 if playerLocated == False:
-                    raise Exception("无效的玩家编号")
+                    raise Exception("无效的玩家编号，你确定你的眼神还好吗？")
                 break
             except Exception as e:
                 msgHandler.warn(str(e))
@@ -123,7 +146,7 @@ while True:
         attackResult = random.randint(0, 100)
         if playerOnStage[2] > attackResult:
             msgHandler.info(
-                f"{attackResult} - 攻击成功：{playerOnStage[1]} 杀死了 {targetPlayer[1]}！"
+                f"{attackResult} - 攻击成功：{playerOnStage[1]} 让 {targetPlayer[1]}鲜血满地！"
             )
             targetPlayer[0] = "x"
             input("按下 Enter 开启下一回合\n>>")
@@ -131,14 +154,28 @@ while True:
             msgHandler.info(
                 f"{attackResult} - 攻击失败：{playerOnStage[1]} 尝试杀死 {targetPlayer[1]} 时挥空了！"
             )
+            sleep(0.2)
+            if attackResult >= targetPlayer[3]:
+                msgHandler.info(
+                    f"转折点：{playerOnStage[1]} 尝试杀死 {targetPlayer[1]} 时触发了 TA 的求生本能，现在是，反击时间！"
+                )
+                sleep(0.5)
+                counterAttackResult = random.randint(0, 100)
+                if targetPlayer[2] < counterAttackResult:
+                    msgHandler.info(
+                        f"{playerOnStage[1]} 居然被 {targetPlayer[1]} 反杀…… 有趣啊……"
+                    )
+                    playerOnStage[0] = "x"
+                else:
+                    msgHandler.info(
+                        f"{playerOnStage[1]} 安然无恙…… 暂时的。不知道{targetPlayer[1]}是否会伺机寻仇呢？"
+                    )
             input("按下 Enter 开启下一回合\n>>")
             roundCount -= -1
     if gameOver:
         break
 
 msgHandler.banner("Chivalry's Oath - 游戏结束")
-msgHandler.info(f"游戏结束！赢家是：({winner[0]}): {winner[1]} - {winner[2]}！")
+msgHandler.info(f"游戏结束！浴血的那位赢家是：({winner[0]}): {winner[1]} - {winner[2]}！")
 
-input("按下 Enter 退出游戏。\n>>")
-
-# TODO: 弹反机制
+input("按下 Enter 退出游戏。…… 你真的退出了…… 吗？\n>>")
